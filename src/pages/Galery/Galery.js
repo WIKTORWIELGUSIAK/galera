5e9e8693285e924d1c122c9fe8e0bd9dd63012e0
Wiktor <witia16@gmail.com>
2022-04-26 02:38:55 +0200

    remove: Remove file with backup Galery-backup.js.
    refactor: Feat UseContext to aplication instead of traditional props.
    
    style: Add animations and new style to navbar.
    
    fix: Fix problem with adding to data base incorrect data with roads.
    
    feat: Block part of functions for unlogged users

--- src/pages/Galery/Galery.js
+++ src/pages/Galery/Galery.js
@@ -1,8 +1,15 @@
 /** @format */
 
-import React, { useCallback, useEffect, useState } from "react";
-import { db } from "../../firebase-config";
+import React, {
+  useCallback,
+  useEffect,
+  useState,
+  useContext,
+  createContext,
+} from "react";
+import { db, auth } from "../../firebase-config";
 import { doc, deleteDoc } from "firebase/firestore";
+import { AppContext } from "../../App";
 
 import {
   Wrapper,
@@ -20,11 +27,25 @@ import {
 } from "./GaleryElements";
 import useFirebase from "../../hooks/useFirebase";
 
-export default function Galery({ categories, setCategories }) {
+export const GaleryContext = createContext();
+
+export default function Galery() {
+  const app = useContext(AppContext);
+
   const [images, setImages] = useState([]);
   const [deleted, setDeleted] = useState(true);
   const [modalState, setModalState] = useState(false);
   const [modalImage, setModalImage] = useState("");
+  const galeryValue = {
+    images: images,
+    setImages: setImages,
+    deleted: deleted,
+    setDeleted: setDeleted,
+    modalState: modalState,
+    setModalState: setModalState,
+    modalImage: modalImage,
+    setModalImage: setModalImage,
+  };
 
   const deleteImg = async (e) => {
     const imgDoc = doc(db, "images", e.target.id);
@@ -50,44 +71,48 @@ export default function Galery({ categories, setCategories }) {
     document.addEventListener("keydown", keyPress);
   }, [keyPress]);
 
-  useFirebase(setCategories, setImages, deleted);
+  useFirebase(app.setCategories, setImages, deleted);
 
   return (
-    <Wrapper>
-      {categories.map((category) => {
-        return (
-          <Category key={category.category}>
-            <Description>{category.category}</Description>
-            <Images>
-              {images
-                .filter((el) => el.category === category.category)
-                .map((image) => {
-                  return (
-                    <SingleImage id={image.id} key={image.id}>
-                      <Image
-                        onClick={openModal}
-                        src={`https://res.cloudinary.com/galera-kajaki/image/upload/v1647894863/${image.link}.jpg`}
-                      />
-                      <DelBtnHolder>
-                        <DelBtnBg
-                          id={image.id}
-                          onClick={(e) => deleteImg(e)}
-                        ></DelBtnBg>
-                        <Trash />
-                      </DelBtnHolder>
-                    </SingleImage>
-                  );
-                })}
-            </Images>
-          </Category>
-        );
-      })}
-      {modalState ? (
-        <Modal>
-          <ModalBg onClick={closeModal}></ModalBg>
-          <ImageModal src={modalImage} />
-        </Modal>
-      ) : null}
-    </Wrapper>
+    <GaleryContext.Provider value={galeryValue}>
+      <Wrapper>
+        {app.categories.map((category) => {
+          return (
+            <Category key={category.category}>
+              <Description>{category.category}</Description>
+              <Images>
+                {images
+                  .filter((el) => el.category === category.category)
+                  .map((image) => {
+                    return (
+                      <SingleImage id={image.id} key={image.id}>
+                        <Image
+                          onClick={openModal}
+                          src={`https://res.cloudinary.com/galera-kajaki/image/upload/v1647894863/${image.link}.jpg`}
+                        />
+                        {app.user !== null ? (
+                          <DelBtnHolder>
+                            <DelBtnBg
+                              id={image.id}
+                              onClick={(e) => deleteImg(e)}
+                            ></DelBtnBg>
+                            <Trash />
+                          </DelBtnHolder>
+                        ) : null}
+                      </SingleImage>
+                    );
+                  })}
+              </Images>
+            </Category>
+          );
+        })}
+        {modalState ? (
+          <Modal>
+            <ModalBg onClick={closeModal}></ModalBg>
+            <ImageModal src={modalImage} />
+          </Modal>
+        ) : null}
+      </Wrapper>
+    </GaleryContext.Provider>
   );
 }
